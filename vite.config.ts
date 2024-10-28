50e6a572cc80e9d053d50e7a71b7c3696b2b50a7
Boran Cui <boran.cui@outlook.com>
2024-10-28 20:49:06 +0800

    NEW

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/marvincui.github.io/', // 将这个路径替换为你的仓库名
 });
