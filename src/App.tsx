55f5f2bd495764898c81aaec018f36e80d4b5c1a
Boran Cui <boran.cui@outlook.com>
2024-12-02 21:11:02 +0800

    NEW

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import FirstArticle from './pages/FirstArticle';
 import SecondArticle from './pages/SecondArticle';
